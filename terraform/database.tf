adaea149174c83000d9c680fbbea517152ac440c
michellecfino <ma.casallas@uniandes.edu.co>
2026-04-04 23:46:37 -0500

    ;-;

--- terraform/database.tf
+++ terraform/database.tf
@@ -27,7 +27,7 @@ resource "aws_security_group" "rds_sg" {
 resource "aws_db_instance" "postgres_db" {
   identifier           = "biteco-db-escalabilidad"
   engine               = "postgres"
-  engine_version       = "15.4" # Versión estable y moderna
+  engine_version       = "16.1"
   instance_class       = "db.t3.micro"
   allocated_storage    = 20
   db_name              = "bit_db"
