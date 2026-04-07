d36aa543fc9ad8cae1f85102375c55d6b2fd34ab
michellecfino <ma.casallas@uniandes.edu.co>
2026-04-07 05:45:29 -0500

    quitar duplicado del output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -4,10 +4,3 @@ output "kong_url" {
 output "alb_dns" {
   value = aws_lb.app_lb.dns_name
 }
-output "kong_url" {
-  value = "http://${aws_instance.kong.public_ip}:8000"
-}
-
-output "alb_url" {
-  value = "http://${aws_lb.app_lb.dns_name}:8000"
-}
