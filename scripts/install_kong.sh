ed0fb0434c04b527c0d405e339b87b56768664f2
michellecfino <ma.casallas@uniandes.edu.co>
2026-04-07 04:46:57 -0500

    jejej perdón

--- scripts/install_kong.sh
+++ scripts/install_kong.sh
@@ -6,17 +6,22 @@ apt-get install -y docker.io
 systemctl start docker
 systemctl enable docker
 
-docker network create kong-net
+cat <<EOF > /home/ubuntu/kong.yml
+_format_version: "3.0"
+
+services:
+  - name: reportes
+    url: http://${alb_dns}:8001
+    routes:
+      - name: reportes-route
+        paths:
+          - /reporte-mensual
+EOF
 
 docker run -d --name kong \
-  --network=kong-net \
   -e "KONG_DATABASE=off" \
-  -e "KONG_DECLARATIVE_CONFIG=/kong/declarative/kong.yml" \
-  -e "KONG_PROXY_ACCESS_LOG=/dev/stdout" \
-  -e "KONG_ADMIN_ACCESS_LOG=/dev/stdout" \
-  -e "KONG_PROXY_ERROR_LOG=/dev/stderr" \
-  -e "KONG_ADMIN_ERROR_LOG=/dev/stderr" \
-  -e "KONG_ADMIN_LISTEN=0.0.0.0:8001" \
-  -p 8000:8000 -p 8001:8001 \
-  -v /home/ubuntu/kong.yml:/kong/declarative/kong.yml \
+  -e "KONG_DECLARATIVE_CONFIG=/kong.yml" \
+  -p 8000:8000 \
+  -p 8001:8001 \
+  -v /home/ubuntu/kong.yml:/kong.yml \
   kong/kong-gateway:3.0
