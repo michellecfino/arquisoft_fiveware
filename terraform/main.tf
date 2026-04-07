e57f7dc8b598d70966291a045ca660a6bc930cdf
michellecfino <ma.casallas@uniandes.edu.co>
2026-04-07 04:52:55 -0500

    cambios en el ami

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,12 +8,17 @@ provider "aws" {
 data "aws_ami" "ubuntu" {
   most_recent = true
 
+  owners = ["099720109477"] # Canonical
+
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-noble-24.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
 
-  owners = ["099720109477"]
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 }
 
 # ========================
