ab62b818d8c54379a646c629a17cb829ddaa309f
Ariana Lozano Parga <al.lozanop1@uniandes.edu.co>
2026-05-04 12:26:21 -0500

    Add audit path to URL configuration

--- biteco/manejador_reportes/manejador_reportes/urls.py
+++ biteco/manejador_reportes/manejador_reportes/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("reportes.urls")),
+    path("audit/", include("manejador_logs.urls")), 
 ]
