4744d784b828cf5b14b30b03557685ef73e4263f
Ariana Lozano Parga <al.lozanop1@uniandes.edu.co>
2026-05-04 13:59:23 -0500

    Remove admin URL path from urlpatterns

--- biteco/manejador_logs/manejador_logs/urls.py
+++ biteco/manejador_logs/manejador_logs/urls.py
@@ -1,8 +1,5 @@
-# ~/arquisoft_fiveware/biteco/manejador_logs/manejador_logs/urls.py
-from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('logs.urls')),
+    path('', include('logs.urls')), 
 ]
