5799958eed7d3c260117a96677fbe672cfd0e060
lozparga <al.lozanop1@uniandes.edu.co>
2026-05-06 14:44:44 -0500

    token

---
+++ biteco/scripts/generar_tokens.py
@@ -0,0 +1,24 @@
+import jwt
+import time
+
+private_key = "superclave_prueba_123456"
+
+users = [
+    {"sub": "admin", "role": "ADMIN"},
+    {"sub": "user", "role": "USER"}
+]
+
+tokens = []
+for user in users:
+    payload = {
+        "sub": user["sub"],
+        "aud": "https://biteco/api",
+        "role": user["role"],
+        "iat": int(time.time()),
+        "exp": int(time.time()) + 3600
+    }
+    token = jwt.encode(payload, private_key, algorithm="HS256")
+    tokens.append(token)
+
+for t in tokens:
+    print(t)
