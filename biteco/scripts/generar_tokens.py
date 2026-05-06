883ca2217d38b3d256b23b254628245227b59203
lozparga <al.lozanop1@uniandes.edu.co>
2026-05-06 14:51:32 -0500

    Actualizar kong.yml para JWT testing

--- biteco/scripts/generar_tokens.py
+++ biteco/scripts/generar_tokens.py
@@ -11,12 +11,13 @@ users = [
 tokens = []
 for user in users:
     payload = {
-        "sub": user["sub"],
-        "aud": "https://biteco/api",
-        "role": user["role"],
-        "iat": int(time.time()),
-        "exp": int(time.time()) + 3600
-    }
+    "sub": user["sub"],
+    "aud": "https://biteco/api",
+    "iss": "https://biteco/auth/test",  # <-- Agrega esta línea
+    "role": user["role"],
+    "iat": int(time.time()),
+    "exp": int(time.time()) + 3600
+}
     token = jwt.encode(payload, private_key, algorithm="HS256")
     tokens.append(token)
 
