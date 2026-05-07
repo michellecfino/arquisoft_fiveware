eefaf701bdbc5666ef56832e27540797b4fc8155
lozparga <al.lozanop1@uniandes.edu.co>
2026-05-06 19:28:09 -0500

    allowed

--- biteco/manejador_logs/manejador_logs/settings.py
+++ biteco/manejador_logs/manejador_logs/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import boto3  # <--- IMPORTANTE: Agrégalo aquí
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -12,7 +13,7 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 ALLOWED_HOSTS = [
     "3.80.36.86",       # La IP Pública de este servidor (Audit-Server)
     "172.31.20.28",     # La IP Privada de este servidor (Audit-Server)
-    "3.91.97.197",      # La IP Pública de Reportes (por si acaso)
+    "3.91.97.197",      # La IP Pública de Reportes
     "172.31.17.51",     # IP Privada Reportes 1
     "172.31.26.43",     # IP Privada Reportes 2
     "172.31.18.237",    # IP Privada Reportes 3
@@ -22,7 +23,7 @@ ALLOWED_HOSTS = [
 INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.auth",
-    "manejador_logs", # Tu app de auditoría
+    "manejador_logs", 
     "logs",
 ]
 
@@ -32,7 +33,6 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "manejador_logs.urls"
 
-# ESTA ES LA BASE DE DATOS DE AUDITORÍA (RDS ESCONDIDA)
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
@@ -42,6 +42,39 @@ DATABASES = {
         "HOST": os.getenv("DB_HOST", "audit-db.c4mxolen2vp8.us-east-1.rds.amazonaws.com"),
         "PORT": os.getenv("DB_PORT", "5432"),
     }
+}
+
+# --- CONFIGURACIÓN DE LOGS PARA AWS CLOUDWATCH ---
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+        'cloudwatch': {
+            'level': 'INFO',
+            'class': 'watchtower.CloudWatchLogHandler',
+            'boto3_client': boto3.client('logs', region_name='us-east-1'),
+            'log_group': 'BITECO-Audit-Logs',
+            'stream_name': 'audit-server-stream',
+            'create_log_group': True,
+            'send_interval': 1,       # Envío casi instantáneo
+            'max_batch_count': 1,     # No esperar a acumular logs
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console', 'cloudwatch'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+        'manejador_logs': {  # Asegúrate que este nombre coincida con tu app
+            'handlers': ['console', 'cloudwatch'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+    },
 }
 
 TIME_ZONE = "UTC"
