8726a1353202536d36eda3bbcd9fcc0927688c35
michellecfino <ma.casallas@uniandes.edu.co>
2026-05-10 16:29:06 -0500

    ahora sin balanceador

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -63,14 +63,7 @@ output "ec2_security_group_id" {
   value       = aws_security_group.ec2_sg.id
 }
 
-# -------------------------------------------------------
-# Load Balancer
-# -------------------------------------------------------
 
-output "alb_dns_name" {
-  description = "Public DNS of the ALB (HTTP :80) -> Kong :8000 on each instance"
-  value       = aws_lb.main.dns_name
-}
 
 # -------------------------------------------------------
 # Network Outputs
