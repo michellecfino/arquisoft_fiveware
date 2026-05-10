3460c743c0ab9eb6afde5e39f73bb7b940fb1607
michellecfino <ma.casallas@uniandes.edu.co>
2026-05-10 12:36:58 -0500

    ;-;

--- terraform/main.tf
+++ terraform/main.tf
@@ -307,7 +307,7 @@ resource "aws_db_subnet_group" "rds_subnet_group" {
 resource "aws_db_instance" "postgres" {
   identifier             = "${var.project_name}-postgres"
   engine                 = "postgres"
-  engine_version         = "16.3"
+  engine_version         = "16.1"
   instance_class         = var.rds_instance_class
   allocated_storage      = 20
   max_allocated_storage  = 100          # autoscaling storage up to 100 GB
