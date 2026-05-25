3a63675158d36113590948eac1b623601deef7eb
Ariana Lozano <al.lozanop1@uniandes.edu.co>
2026-05-24 23:23:07 -0500

    terraform

--- biteco/deployments/terraform/db.tf
+++ biteco/deployments/terraform/db.tf
@@ -1,22 +1,24 @@
-resource "aws_db_instance" "database" {
-  identifier             = "rds-postgresql-latencia"
-  engine                 = "postgres"
-  instance_class         = "db.t3.micro"
-  allocated_storage      = 20
-  username               = "postgres"
-  password               = "postgres123"
-  db_name                = "biteco"
-  skip_final_snapshot    = true
-  publicly_accessible    = true
-  vpc_security_group_ids = [aws_security_group.traffic_db.id]
+# Security Group para la base de datos
+resource "aws_security_group" "traffic_db" {
+  name        = "biteco-traffic-db"
+  description = "Allow PostgreSQL access"
 
-  tags = merge(local.common_tags, {
-    Name = "rds-postgresql-latencia",
-    Role = "database"
-  })
-}
+  ingress {
+    description = "Traffic to PostgreSQL"
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-output "database_endpoint" {
-  description = "RDS PostgreSQL endpoint"
-  value       = aws_db_instance.database.address
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "biteco-traffic-db"
+  }
 }
